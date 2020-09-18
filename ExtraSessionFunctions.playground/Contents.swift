6afa55395d3ac59e9965f3b24d66c59a7cc842a7
Omar <omsi96@users.noreply.github.com>
2020-09-18 18:28:21 +0300

    Add files via upload

---
+++ ExtraSessionFunctions.playground/Contents.swift
@@ -0,0 +1,129 @@
+import UIKit
+
+/*: 1. Create a a function that greets the person by saying Hello to their name.
+ ```
+ sayHello(name: "Omar") // Output:  Hello Omar
+ ```
+ */
+
+
+
+
+
+/*: 2. Create a a function that greets the person by saying (Hello) to their names, or "Hello"'s equivalent translation in other languages. Choose at least 3 languages.
+ 
+ > ***(Bonus!)**: If the language is not provided, english is the default value.
+```
+ sayHello(name: "Omar", language: "English") // Output:  Hello Omar
+ sayHello(name: "Omar", language: "French") // Output:  Bonjour Omar
+ sayHello(name: "Omar", language: "Turkish") // Output:  Merhaba Omar
+
+```
+*/
+
+
+
+
+
+
+
+
+/*:
+3. Create a function that will take your birth year, and calculates your age
+ > ***Bonus!**: Insert months and days.
+```
+ calculateAge(birthYear: 1996) // Output: 24
+```
+*/
+
+
+
+
+/*: 4. Create a a function that takes a parameter of a person's name, and it **returns** the meaning of the name. (do at least 3 names)
+```
+ print(meaning(name: "رتاج")) // Output:  الباب العظيم
+ print(meaning(name: "حيدر")) // Output:  الأسد الجميل
+ print(meaning(name: "غدير")) // Output:  المطر
+
+```
+*/
+
+
+
+
+
+
+
+/*: 5. Create a a function that takes a parameter of a person's name, and it **returns** the meaning of the name. (do at least 3 names)
+ ***((BONUS))**: Create it using dictionaries
+```
+ print(meaning(name: "رتاج")) // Output:  الباب العظيم
+ print(meaning(name: "حيدر")) // Output:  الأسد الجميل
+ print(meaning(name: "غدير")) // Output:  المطر
+
+```
+*/
+
+
+
+
+
+
+
+/*:
+ 6. Create a random joke function that returns a random joke from an array stored inside the function
+ ```
+ print(randomJoke()) // Output: مرة واحد راح للبقالة قالوه عندك جبنة بيضة قالو لا وهو عنده قوه
+ ```
+ */
+
+
+
+
+
+/*:
+7. Create a function that converts KGs to pounds. And a function that converts pounds to KGs
+ > 1 KG = 0.453592
+```
+ print(poundToKilo(4)) // Output: 1.814368
+ print(KiloToPound(4)) // Output: 8.81849
+
+```
+*/
+
+
+
+
+
+
+/*:
+8. Create a function that takes an array of integer, and it returns an array with grades that are only higher then 90!
+ > ***SUPER DUPER** Use `filter` method!
+```
+ print(above90([60, 70, 80, 90, 92, 91])) // Output: [90, 92, 91]
+
+```
+*/
+
+
+
+
+
+
+/*:
+ 
+9. Create a function that converts multiple KGs to pounds. It takes array of Kilos, and returns array of pounds
+ 
+> 1 KG = 0.453592
+ 
+ > ***SUPER DUPER** Use `map` method!
+```
+ print(poundsToKilos([4,5,6,7])) // Output: [1.81437, 2.26796, 2.72155, 3.17515]
+
+```
+*/
+
+
+
+
+
